6748cd8d0a96d676d54941a2feceeb2fee53160e
asz3740 <49752744+asz3740@users.noreply.github.com>
2020-05-11 13:10:39 +0900

    Arrangement

---
+++ 2DPlatformer/Assets/Scripts/Inventory/InventoryUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+
+    public GameObject inventoryPanel;
+    private bool activeInventory = false;
+    
+    void Start()
+    {
+        inventoryPanel.SetActive(activeInventory);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            activeInventory = !activeInventory;
+            inventoryPanel.SetActive(activeInventory);
+        }
+    }
+}
