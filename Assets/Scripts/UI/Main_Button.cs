d14d1b29ae38a64fdddd7c0a77b5ac8e78c6ca3b
정훈 <41151326+pocari-tea@users.noreply.github.com>
2020-06-17 15:52:12 +0900

    catruto

---
+++ Assets/Scripts/UI/Main_Button.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Button : MonoBehaviour
+{
+    public void ClickStart()
+    {
+        LoadingSceneManager.LoadScene("Stage1");
+    }    
+    
+    public void ClickExit()
+    {
+        Application.Quit();
+    }    
+}
